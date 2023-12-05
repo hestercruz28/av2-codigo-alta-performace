325fbace237fca25162c674101245995c6568fff
hestercruzz28 <hestercruzcruz@gmail.com>
2023-12-04 21:06:34 -0300

    meu primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,85 +1,21 @@
-import { useState } from 'react';
-import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulario';
-import Time from './componentes/Time';
-// import axios from 'axios';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import './App.css';
+import Header from './componentes/Header';
+import Inicio from './pages/Inicio';
+import Preenchimento from './pages/Preenchimento';
 
 
 function App() {
-  // const [data, setData] = useState([]);
-
-  
-  // useEffect(() => {
-  //   // Substitua a URL abaixo pelo URL do seu servidor Node.js
-  //   axios.get('http://localhost:3001/user')
-  //     .then((response) => {
-  //       setData(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }, []);
-
-  // console.log('data=', data);
-
-  const times = [
-    {
-      nome: 'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
-    },
-    {
-      nome: 'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
-    },
-    {
-      nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
-    },
-    {
-      nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
-    },
-    {
-      nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
-    },
-    {
-      nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
-    },
-    {
-      nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
-    }
-  ]
-
-  const [colaboradores, setColaboradores] = useState([])
-
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
-    setColaboradores([...colaboradores, colaborador])
-  }
 
   return (
-    <div className="App">
-      <Banner />
-
-      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-
-      {times.map(time => <Time 
-        key={time.nome} 
-        nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria} 
-        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
-      />)}   
+    <div>
+      <BrowserRouter>
+          <Header useNavigate={useNavigate} />
+              <Routes>
+                  <Route path="/" element={<Inicio useNavigate={useNavigate} />} />
+                  <Route path="/preenchimento" element={<Preenchimento useNavigate={useNavigate} />} />
+              </Routes>
+      </BrowserRouter>
 
     </div>
   );
